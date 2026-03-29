0d8f88e5a5688a4a0c7b9551c76114fca03a2e11
robentialkinetics-kishtech <robentialkinetics@gmail.com>
2026-03-29 18:38:31 +0530

    Delete version_1_YOLO_ONLY directory

--- version_1_YOLO_ONLY/data/__init__.py
+++
@@ -1,6 +0,0 @@
-# Data management package initialization
-import os
-
-# Create necessary directories if they don't exist
-os.makedirs("data/programs", exist_ok=True)
-os.makedirs("data/logs", exist_ok=True)
